8f6fa9af294d19bedd1a803d2f3f99a533e6e645
Seol Cheon <jojo432143@gmail.com>
2020-06-14 12:34:41 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+//IIFE -Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("Seol's App Started...");
+
+    }
+
+    window.addEventListener("load",Start);
+
+})();
